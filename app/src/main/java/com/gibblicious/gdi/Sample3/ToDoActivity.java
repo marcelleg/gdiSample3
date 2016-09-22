6c22ba27ec78bddd5412ee89de326441cf719626
Marcelle Gibble <marcelle.gibble@ticketmaster.com>
2016-09-22 11:13:57 -0400

    add preferences

--- app/src/main/java/com/gibblicious/gdi/Sample3/ToDoActivity.java
+++ app/src/main/java/com/gibblicious/gdi/Sample3/ToDoActivity.java
@@ -1,5 +1,7 @@
 package com.gibblicious.gdi.Sample3;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,12 +12,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
 import java.util.List;
 import java.util.Arrays;
 
 public class ToDoActivity extends AppCompatActivity {
 
-    List<String> toDoList = Arrays.asList(
+    private static final List<String> toDoList = Arrays.asList(
             "Go to Harris Teeter",
             "Wash the car",
             "Buy Grant a birthday present",
@@ -29,6 +33,8 @@ public class ToDoActivity extends AppCompatActivity {
             "prepare for intro to Android session 4",
             "simplify your life with fewer lists",
             "deposit check");
+
+    private static final String PREF_IS_ANDROID_USER = "com.gibblicious.gdi.sample3.isAndroidUser";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,6 +65,7 @@ public class ToDoActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+
     private void setupViews() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -75,11 +82,45 @@ public class ToDoActivity extends AppCompatActivity {
         //populate RecyclerView
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.my_todo_list);
 
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(linearLayoutManager);
+        LinearLayoutManager layoutMgr = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutMgr);
 
         ToDoAdapter adapter = new ToDoAdapter(toDoList);
         recyclerView.setAdapter(adapter);
     }
+
+
+
+    public void onNoClick(View v) {
+        savetoPreferences(false);
+        displayPreference();
+    }
+
+    public void onYesClick(View v) {
+        savetoPreferences(true);
+        displayPreference();
+    }
+
+
+    private void savetoPreferences(boolean isAndroidUser) {
+        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+
+        editor.putBoolean(PREF_IS_ANDROID_USER, isAndroidUser);
+        editor.commit();
+    }
+
+
+    private boolean getIsAndroidUserPreference() {
+        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+        return sharedPref.getBoolean(PREF_IS_ANDROID_USER, true);
+    }
+
+    private void displayPreference() {
+        TextView tv = (TextView) findViewById(R.id.response);
+        String s = "Response: " + String.valueOf(getIsAndroidUserPreference());
+        tv.setText(s);
+    }
+
 
 }
